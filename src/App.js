389383ff71fbb8c1a75d1c2753c84e3de099f978
Jeremy Greer <jex.grizzle@gmail.com>
2019-06-27 19:08:05 -0700

    Basic list

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const DataRow = (props) => (
+  <div className="DataRow">
+    <div className="Text">{props.text}</div>
+    <div className="Controls">
+      <button onClick={props.onSelect}>Select</button>
+      <button onClick={props.onDelete}>Delete</button>
+    </div>
+  </div>
+)
+
+const List = (props) => (
+  <div className="List">
+    { props.items.map((item) => (
+      <DataRow
+        key={item.id}
+        {...item}
+      />
+    ))}
+  </div>
+)
+
+const items = [
+  { id: 'a', text: 'Foo' },
+  { id: 'b', text: 'Bar' },
+  { id: 'c', text: 'Baz' },
+]
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <List items={items} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
